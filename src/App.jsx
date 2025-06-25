5184b6efbd1fe6ec45c95d100e6696eb2a799c58
Pratikshya <pratikshyagochhayat@gmail.com>
2025-06-25 19:01:17 +0530

    feat: integrate tailwind css and shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-4xl">App</h1>
+      <Button className="bg-green-600 mt-5">Add Todo</Button>
+    </div>
+  );
 }
 
 export default App;
